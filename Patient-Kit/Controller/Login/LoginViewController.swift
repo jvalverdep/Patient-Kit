cd3b40eff08f83a6b2159998388f5c41857c9eb3
zcrome <soportezcrome@gmail.com>
2018-05-14 13:40:46 -0500

    Structure and folder creation

---
+++ Patient-Kit/Controller/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Patient-Kit
+//
+//  Created by Melanie on 5/14/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
